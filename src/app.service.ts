d92bc802462b0e56e75af16b3c380baf510ac77e
DanielOS1 <danielolivares60@gmail.com>
2025-06-29 16:11:09 -0400

    initial commit

---
+++ src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getMessage(): string {
+    return 'Aplicación de ejemplo con NestJS para Devops y CICD';
+  }
+}
